510274e66432a881f69ef720d2ee4e16db7481bd
atrem.bezruk <artem.bezruk@gmail.com>
2023-06-11 09:36:40 +0300

    Seminar#01 home work

---
+++ 01_Intro/homeWork/hw_01_task1/src/Main.java
@@ -0,0 +1,42 @@
+// Урок 1. Знакомство с языком программирования Java
+// Задание 1
+// Вычислить n-ое треугольного число (сумма чисел от 1 до n)
+// n! (произведение чисел от 1 до n)
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int num = GetConsoleInputInt("Please enter integer value");
+        System.out.println(num);
+
+    }
+
+    public static int GetConsoleInputInt(String requestText)
+    {
+        return GetConsoleInputInt(requestText, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static int GetConsoleInputInt(String requestText, int startRange, int endRange)
+    {
+        Scanner consoleInput = new Scanner(System.in);
+        int value;
+        do
+        {
+            System.out.println(requestText);
+            while (!consoleInput.hasNextInt())
+            {
+                System.out.printf("Entered value is not integer type. %s\n", requestText);
+                consoleInput.next();
+            }
+            value = consoleInput.nextInt();
+            if (value < startRange || value > endRange)
+            {
+                System.out.printf("Entered value is out of %d...%d range. %s\n", startRange, endRange, requestText);
+            }
+        } while (value < startRange || value > endRange);
+        return value;
+    }
+}
