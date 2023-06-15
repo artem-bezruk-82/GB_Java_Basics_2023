897673d2edfeef71b1e92061f127ad681b54982c
atrem.bezruk <artem.bezruk@gmail.com>
2023-06-15 17:32:23 +0300

    Module#01 home work task#3

--- 01_Intro/homeWork/hw_01_task3/src/Main.java
+++ 01_Intro/homeWork/hw_01_task3/src/Main.java
@@ -1,17 +1,68 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Курс: Java. Знакомство и как пользоваться базовым API
+// Урок 2. Знакомство с языком программирования Java
+// Домашняя работа
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+// Задание 3
+// Реализовать простой калькулятор
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double valueLeft = GetConsoleInputDouble("Please enter first value");
+        System.out.println(valueLeft);
+
+        System.out.println("Please enter operation [+, -, *, /]");
+        Scanner consoleInput = new Scanner(System.in);
+        char operation = (consoleInput.next().trim().toCharArray())[0];
+
+        double valueRight = GetConsoleInputDouble("Please enter second value");
+        System.out.println(valueRight);
+
+        try
+        {
+            double result = GetCalculationResult(valueLeft, valueRight, operation);
+            System.out.printf("%f %s %f = %f", valueLeft, operation, valueRight, result);
+        }
+        catch (Exception exc)
+        {
+            System.out.println(exc.getMessage());
         }
     }
+
+    public static double GetCalculationResult (double valueLeft, double valueRight, char operation) throws Exception
+    {
+        return switch (operation)
+        {
+            case '+' ->
+                valueLeft + valueRight;
+            case '-'->
+                valueLeft - valueRight;
+            case '*'->
+                valueLeft * valueRight;
+            case '/'->
+                valueLeft / valueRight;
+            default ->
+                throw new Exception("Invalid operation. The only +, -, *, / are supported");
+        };
+    }
+
+
+    public static double GetConsoleInputDouble(String requestText)
+    {
+        Scanner consoleInput = new Scanner(System.in);
+
+        System.out.println(requestText);
+        while (!consoleInput.hasNextDouble())
+        {
+            System.out.printf("Entered value is not a number. %s\n", requestText);
+            consoleInput.next();
+        }
+        return consoleInput.nextInt();
+    }
+
 }
+
