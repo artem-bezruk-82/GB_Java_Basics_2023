0f4edd8f2a8539aa74f7641997739f1c4040b4fc
atrem.bezruk <artem.bezruk@gmail.com>
2023-06-15 14:52:14 +0300

    Module#01 home work task#2

--- 01_Intro/homeWork/hw_01_task2/src/Main.java
+++ 01_Intro/homeWork/hw_01_task2/src/Main.java
@@ -1,17 +1,29 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// Курс: Java. Знакомство и как пользоваться базовым API
+// Урок 1. Знакомство с языком программирования Java
+// Домашняя работа
+// Задание 2
+// Вывести все простые числа от 1 до 1000
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i < 1000; i++) {
+            if (IsPrime(i))
+                System.out.println(i);
         }
+    }
+
+    public static boolean IsPrime(int value)
+    {
+        int val = Math.abs(value);
+        if (val < 2)
+            return false;
+        for (int i = 2; i < Math.sqrt(val); i++)
+        {
+           if (val % i == 0)
+               return false;
+        }
+        return true;
     }
 }
