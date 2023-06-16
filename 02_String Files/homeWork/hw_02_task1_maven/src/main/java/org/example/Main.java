49b381cb616fbd81c6a38bc8dc8d3036e965c822
atrem.bezruk <artem.bezruk@gmail.com>
2023-06-17 00:22:08 +0300

    Seminar#2 home work task#1 corrected

--- 02_String Files/homeWork/hw_02_task1_maven/src/main/java/org/example/Main.java
+++ 02_String Files/homeWork/hw_02_task1_maven/src/main/java/org/example/Main.java
@@ -1,3 +1,16 @@
+// Курс: Java. Знакомство и как пользоваться базовым API
+// Урок 2. Строки. Файлы. Исключения.
+// Домашняя работа
+
+// Задание 1
+// Дана строка sql-запроса "select * from students WHERE ".
+// Сформируйте часть WHERE этого запроса, используя StringBuilder.
+// Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// String input_str = "{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}"
+// Ввод данных: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
+
 package org.example;
 
 import com.google.gson.Gson;
