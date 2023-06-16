923aa4492b05e41794004d6ed8717163869ec44b
atrem.bezruk <artem.bezruk@gmail.com>
2023-06-16 14:58:59 +0300

    Seminar#2 class work task#1 done

---
+++ 02_String Files/classWork/cw_02_task1/src/Main.java
@@ -0,0 +1,27 @@
+// Курс: Java. Знакомство и как пользоваться базовым API
+// Урок 2. Знакомство с языком программирования Java
+
+// Задание 1
+// Дано четное число N>0 и символы c1 и c2.
+// Написать метод, который вернет строку длины N, которая сотоит из чередующихся символов c1 и c2, насия с c2.
+
+import java.lang.StringBuilder;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println(GetInterleavedSequence('#', '@', 10));
+    }
+
+    public static String GetInterleavedSequence(char symbolFirst, char symbolSecond, int length)
+    {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < sb.capacity(); i++) {
+            sb.append(i % 2 == 0 ? symbolFirst : symbolSecond);
+        }
+        return sb.toString();
+    }
+
+
+}
